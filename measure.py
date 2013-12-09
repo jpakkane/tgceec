89a289e2b59e10fa8fb922e1d1e7bd2bb40bf3cd
Jussi Pakkanen <jpakkane@gmail.com>
2013-12-09 21:55:22 +0200

    Validate plain entries.

--- measure.py
+++ measure.py
@@ -3,11 +3,27 @@
 from glob import glob
 import os
 import subprocess
+import re
 
 def plainchecker(infile):
+    permitted = {'vector' : True,
+                 'map' : True,
+                 'iostream' : True,
+                 }
+    includere = re.compile('''^\s*#\s*include\s*[<"](.*?)[>"]''')
+    for line in open(infile):
+        if '#define' in line:
+            print("Invalid use of preprocessor detected in", infile)
+            return False
+        m = re.search(includere, line)
+        if m:
+            include = m.group(1)
+            if include not in permitted:
+                print("Invalid include", include, "in", infile)
+                return False
     return True
 
-def measure(subdir, checker):
+def measure(subdir):
     compiler = '/usr/bin/g++'
     basic_flags = ['-std=c++11', '-c', '-o', '/dev/null']
     results = []
@@ -15,6 +31,10 @@ def measure(subdir, checker):
         basename = os.path.split(d)[-1]
         sourcename = basename + '.cpp'
         fullsrc = os.path.join(d, sourcename)
+        if subdir == 'plain':
+            checker = plainchecker
+        if not checker(fullsrc):
+            continue
         if not os.path.isfile(fullsrc):
             print("Bad file in subdir", d)
             continue
@@ -33,7 +53,8 @@ def measure(subdir, checker):
     return results
 
 def run():
-    plain_times = measure('plain', plainchecker)
+    print('Starting measurements for type plain')
+    plain_times = measure('plain')
     print('Table for category plain:\n')
     for i in plain_times:
         print(i[0], i[1], i[2], i[3])
