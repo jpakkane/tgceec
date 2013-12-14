4569e2aa1be86d03c0cc139e1e0caf63a01d54f4
Jussi Pakkanen <jpakkane@gmail.com>
2013-12-14 21:32:41 +0200

    Added GPL header.

--- measure.py
+++ measure.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+#  Copyright (C) 2013 Jussi Pakkanen
+#
+#  Authors:
+#     Jussi Pakkanen <jpakkane@gmail.com>
+#
+#  This library is free software; you can redistribute it and/or modify it under
+#  the terms of version 3 of the GNU General Public License as published
+#  by the Free Software Foundation.
+#
+#  This library is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#  details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from glob import glob
 import os
 import subprocess
