bab915bde9726f142d44fc7b3327b7fea4566023
Jussi Pakkanen <jpakkane@gmail.com>
2013-12-16 21:22:32 +0200

    Quote the input file name.

--- measure.py
+++ measure.py
@@ -136,6 +136,9 @@ def measure(subdir):
         basename = os.path.split(d)[-1]
         sourcename = basename + '.cpp'
         fullsrc = os.path.join(d, sourcename)
+        if "'" in fullsrc:
+            print('File name must not contain an apostrophe.')
+            continue
         if has_extra_files(d, basename):
             continue
         if not os.path.isfile(fullsrc):
@@ -163,7 +166,7 @@ def measure(subdir):
         if not os.path.isfile(fullsrc):
             print("Bad file in subdir", d)
             continue
-        cmd_arr = [compiler, fullsrc] + basic_flags
+        cmd_arr = [compiler, "'%s'" % fullsrc] + basic_flags
         if subdir == 'anything':
             includefile = os.path.join(d, 'includes.txt')
             for line in open(includefile):
@@ -172,6 +175,9 @@ def measure(subdir):
                 cmd_arr.append('-I' + line.strip())
         cmd_arr += ['2>&1', '/dev/null', '|', 'wc', '-c']
         cmd = ' '.join(cmd_arr)
+        # Remember kids, you should not use shell=True unless you
+        # have a very good reason. We need it to use wc, because
+        # measuring with Python makes a copy of the output.
         pc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         res = pc.communicate()
         stdout = res[0].decode()
